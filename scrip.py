d346f3850e7dbcc259893d6011892377dd386c22
unknown <esron.dtamar@gmail.com>
2017-09-26 11:56:07 -0300

    HTTPlib e urlib, conexão com API

--- scrip.py
+++ scrip.py
@@ -1,10 +1,35 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
+# -*- coding: utf8 -*-
 
+# Libraries for http and serial handling
 import serial
+import httplib
+import urllib
 
-ser = serial.Serial('COM4', 115200)
+# Constants
+ServerUrl = "rpmreadapi.herokuapp.com"
+POSTUrl    = "/reads"
+
+ser = serial.Serial('COM9', 115200) # Serial port configuration
+
+conn = httplib.HTTPConnection(ServerUrl) # Server connection
+
+headers = {
+    'content-type': "application/x-www-form-urlencoded",
+    'cache-control': "no-cache",
+    }
+
+params = urllib.urlencode({'motor_number': '1', 'rpm_indutivo': '1', 'rpm_optico': '1'})
 
 while True:
-    response = ser.readline()
-    print "Getting: " + response
+    atmegaResponse = ser.readline()
+    conn.request("POST", POSTUrl, params, headers)
+    results = conn.getresponse()
+    print "Request status: ", results.status, results.reason
+    conn.close()
+
+    while int(results.status) != 200:
+        conn.request("POST", POSTUrl, params, headers)
+        results = conn.getresponse()
+        print "Request status: ", results.status, results.reason
+        conn.close()
--- src/main.c
+++ src/main.c
@@ -7,36 +7,31 @@
 
 unsigned volatile int cont;
 
-ISR (INT0_vect){
+ISR (INT0_vect) {
     cont++;
 }
 
-int main(void){
+int main(void) {
     char string[5];
     cont = 0;
+
     cli();                    // Disable interrupts
     USART_init();
+
     DDRB = 0xFF;
     DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
     PORTD |= (1 << PORTD2);   // turn On the Pull-up
 
-    EICRA |= (1 << ISC00) | (1 << ISC01);    // set INT0 to trigger on ANY logic change
+    EICRA |= (1 << ISC00) | (1 << ISC01);    // set INT0 to trigger on the rising edge
     EIMSK |= (1 << INT0);     // Turns on INT0
 
-    PORTB = 0;
+    sei();                    // Enable interrups
 
-    for (int cont = 0; cont < 1000; cont++) {
-        utoa(cont, string, 10);
-        USART_putString(string);
-        USART_putString("\n");
-    }
-
-    sei(); // Enable interrups
     while(1) {
         _delay_ms(5000);
         utoa(cont, string, 9);
         USART_putString(string);
         USART_putString("\n");
-        //cont = 0;
+        cont = 0;
     }
 }
