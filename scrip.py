c3b48f67bce59fff26e7ed45dd1d7ac3860af5af
esron <esron.dtamar@gmail.com>
2017-09-27 21:53:32 -0300

    Codigo para capturar os dois sensores

--- scrip.py
+++ scrip.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-
-# Libraries for http and serial handling
+"""
+    This scrip promps for two values via serial portt hen sends to a webserver
+    via a API RESTful. The port, baudhate and motor id are givem by the
+    user via comand line
+"""
+# Serial port communication library
 import serial
+
+# HTTP libraries
 import httplib
 import urllib
 
-# Constants
-ServerUrl = "rpmreadapi.herokuapp.com"
-POSTUrl    = "/reads"
+# Comand line argument parser
+import argparse
 
-ser = serial.Serial('COM9', 115200) # Serial port configuration
+# Constants
+ServerUrl     = "rpmreadapi.herokuapp.com"
+POSTUrl       = "/reads"
+pulsesPerTurn = 4
+
+# Arguments configuration
+parser = argparse.ArgumentParser()
+parser.add_argument('port', help='The serial port to communicate.')
+parser.add_argument('motor', help='The motor\'s that is in test.' )
+parser.add_argument('baud', help='Baudhate in the serial port to communicate.')
+
+args = parser.parse_args()
+
+ser = serial.Serial(args.port, args.baud) # Serial port configuration
 
 conn = httplib.HTTPConnection(ServerUrl) # Server connection
 
@@ -19,17 +37,33 @@ headers = {
     'cache-control': "no-cache",
     }
 
-params = urllib.urlencode({'motor_number': '1', 'rpm_indutivo': '1', 'rpm_optico': '1'})
+params = urllib.urlencode({
+    'motor_number': args.motor,
+    'rpm_indutivo': '1',
+    'rpm_optico': '1',
+    'voltage': '0'
+    })
 
 while True:
-    atmegaResponse = ser.readline()
-    conn.request("POST", POSTUrl, params, headers)
-    results = conn.getresponse()
-    print "Request status: ", results.status, results.reason
-    conn.close()
+    sensor = ser.readline().rstrip()
+    value  = ser.readline().rstrip()
+    print "Received. sensor: ", sensor, ", value = ", value
 
-    while int(results.status) != 200:
-        conn.request("POST", POSTUrl, params, headers)
-        results = conn.getresponse()
-        print "Request status: ", results.status, results.reason
-        conn.close()
+    inductiveRPM = (value) / (12 * 60)
+
+    sensor = ser.readline().rstrip()
+    value  = ser.readline().rstrip()
+    print "Received. sensor: ", sensor, ", value = ", value
+
+    opticRPM = (value) / (12 * 60)
+
+    # conn.request("POST", POSTUrl, params, headers)
+    # results = conn.getresponse()
+    # print "Request status: ", results.status, results.reason
+    # conn.close()
+    #
+    # while int(results.status) != 200:
+    #     conn.request("POST", POSTUrl, params, headers)
+    #     results = conn.getresponse()
+    #     print "Request status: ", results.status, results.reason
+    #     conn.close()
--- src/main.c
+++ src/main.c
@@ -5,33 +5,60 @@
 
 #include "usart/usart.h"
 
-unsigned volatile int cont;
+unsigned volatile int counter = 0;
 
+// INT0 interrupt service routine
 ISR (INT0_vect) {
-    cont++;
+    counter++;
+}
+
+// INT1 interrupt service routine
+ISR (INT1_vect) {
+    counter++;
+}
+
+// Sends via serial the count of pulses per sensor type
+void sendPulses(char sensorType) {
+    char string[5];
+
+    if(sensorType == 'i') {
+        // Counting inductive interrups in 5 seconds
+        EIMSK |= (1 << INT0);        // Turns on INT0
+        _delay_ms(5000);
+        EIMSK &= ~(1 << INT0);       // Disable INT0
+        USART_putString("i\n");
+    }
+    else if (sensorType == 'o'){
+        // Counting optic interrups in 5 seconds
+        EIMSK |= (1 << INT1);        // Turns on INT1
+        _delay_ms(5000);
+        EIMSK &= ~(1 << INT1);       // Disable INT1
+        USART_putString("o\n");
+    }
+    utoa(counter, string, 10); // Converts the integer to a string
+    USART_putString(string);     // Send the value via serial port
+    USART_send('\n');        // Sends a '\n' to indicate the end of message
+    counter = 0;
 }
 
 int main(void) {
-    char string[5];
-    cont = 0;
 
-    cli();                    // Disable interrupts
+    cli(); // Disable interrupts
     USART_init();
 
-    DDRB = 0xFF;
     DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
-    PORTD |= (1 << PORTD2);   // turn On the Pull-up
+    DDRD &= ~(1 << DDD3);     // Clear the PD3 pin
+    PORTD |= (1 << PORTD2) | (1 << PORTD3);   // turn On the Pull-up
 
-    EICRA |= (1 << ISC00) | (1 << ISC01);    // set INT0 to trigger on the rising edge
-    EIMSK |= (1 << INT0);     // Turns on INT0
+    // set INT0 and INT1 to trigger on the rising edge
+    EICRA |= (1 << ISC00) | (1 << ISC01) | (1 << ISC11) |  (1 << ISC10);
 
-    sei();                    // Enable interrups
+    sei(); // Enable interrups
 
     while(1) {
-        _delay_ms(5000);
-        utoa(cont, string, 9);
-        USART_putString(string);
-        USART_putString("\n");
-        cont = 0;
+        sendPulses('i');
+        sendPulses('o');
     }
+
+    return 0;
 }
